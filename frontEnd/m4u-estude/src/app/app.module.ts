37a8ebc17f264995cb103cf446d47c994633b1b0
Valdinéia da Silva <82914359+vsilva-code@users.noreply.github.com>
2021-11-10 12:01:11 -0300

    correção erros do upload do frontend

--- frontEnd/m4u-estude/src/app/app.module.ts
+++ frontEnd/m4u-estude/src/app/app.module.ts
@@ -1,25 +1,28 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { AddressComponent } from './address/address.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { ListaStudentsComponent } from './lista-students/lista-students.component';
+import { LoginComponent } from './login/login.component';
+import { ManagementComponent } from './management/management.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { UsersService } from './services/users.service';
 import { StudentComponent } from './student/student.component';
 import { UserComponent } from './user/user.component';
-import { AddressComponent } from './address/address.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
-import { ManagementComponent } from './management/management.component';
-=======
-import { FormsModule } from '@angular/forms';
-import { ListaStudentsComponent } from './lista-students/lista-students.component';
->>>>>>> 0b19664edc79c3bf38691edac2f26d3a9d64a2fe
+
+
 
 @NgModule({
   declarations: [
@@ -31,11 +34,10 @@ import { ListaStudentsComponent } from './lista-students/lista-students.componen
     StudentComponent,
     UserComponent,
     AddressComponent,
-<<<<<<< HEAD
-    ManagementComponent
-=======
-    ListaStudentsComponent
->>>>>>> 0b19664edc79c3bf38691edac2f26d3a9d64a2fe
+    ManagementComponent,
+    ListaStudentsComponent,
+    ListUsersComponent
+
   ],
   imports: [
     BrowserModule,
@@ -44,9 +46,12 @@ import { ListaStudentsComponent } from './lista-students/lista-students.componen
     BrowserAnimationsModule,
     MatToolbarModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatFormFieldModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
